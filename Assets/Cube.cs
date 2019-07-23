58dbc0cc18075ac2ea8acae4f2a5f87080ae208b
naoto-leon <naoto.leon@gmail.com>
2019-07-23 22:33:01 +0900

    Initial commit

---
+++ Assets/Cube.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    [Header("About Wave")]
+
+    [SerializeField]
+    float speed = 20f;
+
+    [SerializeField]
+    int sinHight = 10;
+
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float radian;
+        float theta;
+
+        foreach (var boxcube in GameObject_cube.prefabsballs)
+        {
+            theta = (boxcube.transform.position.z + 1) + Time.time * speed;
+
+            radian = Mathf.Deg2Rad * theta * sinHight;
+
+            float sin = Mathf.Sin(radian);
+
+           boxcube.transform.position = new Vector3(boxcube.transform.position.x, sin, boxcube.transform.position.z);
+        }
+
+    }
+
+
+   
+}
---
+++ Assets/GameObject_cube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObject_cube : MonoBehaviour
+{
+
+public static GameObject[,] prefabsballs;
+
+    [SerializeField]
+    public int Amount = 10;
+
+    [SerializeField]
+    int step = 3;
+
+    [Header("Prefab")]
+    public GameObject prefabboal;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabsballs = new GameObject[Amount, Amount];
+
+        for (int j = 0; j < Amount; j++)
+        {
+
+            for (int i = 0; i < Amount; i++)
+            {
+                prefabsballs[i, j] = Instantiate(prefabboal, new Vector3(j * 3, 0, i * step), Quaternion.identity);
+            }
+
+        }
+    }
+
+
+}
